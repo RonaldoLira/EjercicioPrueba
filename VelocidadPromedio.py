4302d386870ac91e8b3e28cc444ed81d200d99b6
RonaldoLira <46898150+RonaldoLira@users.noreply.github.com>
2019-01-21 09:38:18 -0600

    Ronaldo Estefano Lira Buendia, Grupo 02.

---
+++ VelocidadPromedio.py
@@ -0,0 +1,7 @@
+#Autor: Ronaldo Estefano Lira Buendia
+#Calcula la velocidad promedio e un viaje
+
+t=int(input("Ingresa el tiempo del viaje: "))
+d=int(input("Ingresa la distancia del viaje: "))
+v=(t/d)
+print("La velocidad promedio es: ",v)
